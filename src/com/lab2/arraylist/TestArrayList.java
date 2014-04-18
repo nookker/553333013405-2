4fedcf9ccbf00576e53b089631202a4e1cddcdfb
NOOKKER <NOOKKER@NOOKKER-PC>
2014-04-19 02:49:08 +0700

    ส่งงานlab2

---
+++ src/com/lab2/arraylist/TestArrayList.java
@@ -0,0 +1,20 @@
+package com.lab2.arraylist;
+
+public class TestArrayList {
+		
+		public static void main(String[] args) {
+				ArrayList ar = new ArrayList();
+				ar.add("1");
+				System.out.println(ar);
+				ar.add("2");
+				System.out.println(ar);
+				ar.add("3");
+				System.out.println(ar);
+				ar.add(0,"0");
+				System.out.println(ar);
+				
+		}
+		
+	}
+
+
