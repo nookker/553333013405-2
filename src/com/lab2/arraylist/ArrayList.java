3e8a83cf47a66a1dbc95c11986a4606cfca4f568
NOOKKER <NOOKKER@NOOKKER-PC>
2014-04-30 16:40:27 +0700

    Lab 2

--- src/com/lab2/arraylist/ArrayList.java
+++ src/com/lab2/arraylist/ArrayList.java
@@ -1,54 +1,54 @@
 package com.lab2.arraylist;
 
+import java.util.Arrays;
+
 public class ArrayList {
 
 	private Object elementData[] = new Object[1];
 	private int size = 0;
 
 	private void ensureCapacity(int capacity) {
-			if(capacity > elementData.length){
-				Object arr[] = new Object[elementData.length*2];
-				for (int i = 0; i < size; i++) {
-					arr[i] = elementData[i];
-				}
-				elementData = arr;
+		if (capacity > elementData.length) {
+			Object arr[] = new Object[elementData.length * 2];
+			for (int i = 0; i < size; i++) {
+				arr[i] = elementData[i];
 			}
+			elementData = arr;
+		}
 	}
 
 	public void add(Object e) {
-		add(size,e);
+		add(size, e);
 	}
 
-	public void add(int i,Object e) {
-		ensureCapacity(size+1);
-		for (int j = size-1; j >= i; j--) {
-				elementData[j+1] = elementData[j];
+	public void add(int i, Object e) {
+		ensureCapacity(size + 1);
+		for (int j = size - 1; j >= i; j--) {
+			elementData[j + 1] = elementData[j];
 		}
 		elementData[i] = e;
 		size++;
 	}
-	public String toString() { 
-		String ret = "[";
-		for (int i = 0; i < size; i++) {			
-			if(i == size-1){
-				ret = ret+elementData[i]+"]";
-			}else{
-				ret = ret+elementData[i]+",";
-			}
-		}
-		return ret;
-		 }
 
-	public int size() {				
+	@Override
+	public String toString() {
+		Object arr[] = new Object[size];
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = elementData[i];
+		}
+		return Arrays.toString(arr);
+	}
+
+	public int size() {
 		return size;
 	}
 
 	public void addFirst(Object e) {
-		add(0,e);
+		add(0, e);
 	}
 
 	public void addLast(Object e) {
-		add(size,e);				
+		add(size, e);
 	}
 
 	public Object get(int a) {
@@ -56,13 +56,12 @@ public class ArrayList {
 		return get;
 	}
 
-	public void remove(int i) { 
-		 for (int j = i; j < size - 1; j++) { 
-		 elementData[j] = elementData[j + 1]; 
-		 } 
-		 elementData[size-1] = null; 
-		 size--; 
-		 }
-		
+	public void remove(int i) {
+		for (int j = i; j < size - 1; j++) {
+			elementData[j] = elementData[j + 1];
+		}
+		elementData[size - 1] = null;
+		size--;
+	}
 
 }
--- src/com/lab2/arraylist/TestArrayList.java
+++ src/com/lab2/arraylist/TestArrayList.java
@@ -2,19 +2,38 @@ package com.lab2.arraylist;
 
 public class TestArrayList {
 		
-		public static void main(String[] args) {
-				ArrayList ar = new ArrayList();
-				ar.add("1");
-				System.out.println(ar);
-				ar.add("2");
-				System.out.println(ar);
-				ar.add("3");
-				System.out.println(ar);
-				ar.add(0,"0");
-				System.out.println(ar);
-				
-		}
-		
+	public static void main(String[] args) {
+
+		System.out.println("------------------------ArrayList---------------------------");
+		ArrayList li = new ArrayList();
+		li.add("1");
+		li.add("2");
+		li.add("3");
+		li.add("4");
+		li.add("5");
+		System.out.println(li);
+
+		System.out.println("---------------------ArrayListQueue-------------------------");
+		ArrayListQueue qu = new ArrayListQueue();
+		qu.enQueue("1");
+		qu.enQueue("2");
+		qu.enQueue("3");
+		qu.enQueue("4");
+		qu.enQueue("5");
+		System.out.println(qu);
+
+		System.out.println("---------------------ArrayListStack-------------------------");
+		ArrayListStack st = new ArrayListStack();
+		st.push("1");
+		st.push("2");
+		st.push("3");
+		st.push("4");
+		st.push("5");
+		System.out.println(st);
+
+
+
 	}
+}
 
 
